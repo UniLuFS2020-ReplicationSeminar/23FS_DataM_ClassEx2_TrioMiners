c7c11eda11718cdc4417478adb8cd41b098f41c4
‚jullatt‘ <julien.nicolas@bluewin.ch>
2023-04-17 14:33:03 +0200

    adjust corpus and add docvars

--- 02_Data/data processing.R
+++ 02_Data/data processing.R
@@ -1,25 +1,43 @@
 library(here)
 library(quanteda)
 library(quanteda.textplots)
+library(tidyverse)
 
 
 #load data
 load(here("AI_articles_guardian.RData"))
 
+# cleaning data (removing html tags using regexpr)
+articles_df <- articles_df %>% 
+  mutate(V3 = gsub("<.*?>", "", V3))
 
-# create corpus and inspect top features
+
+
+# create corpus and inspect it
 
 myCorpus <- corpus(articles_df, text_field = "V3")
+
+myCorpus <- corpus(myCorpus, docvars = select(articles_df, V1:V2))
+
 head(summary(myCorpus))
 
-tok2 <- tokens(myCorpus , remove_punct = TRUE, remove_numbers=TRUE, remove_symbols = TRUE, split_hyphens = TRUE, remove_separators = TRUE)
-tok2 <- tokens_remove(tok2, stopwords("en"))
-tok2 <- tokens_wordstem (tok2)
-myDfm <- dfm(tok2)
+# tokenize text and remove noise
 
+tok <- tokens(myCorpus , remove_punct = TRUE, 
+               remove_numbers=TRUE, 
+               remove_symbols = TRUE, 
+               split_hyphens = TRUE, 
+               remove_separators = TRUE)
+tok <- tokens_remove(tok, stopwords("en"))
+tok <- tokens_wordstem(tok)
+
+# create dfm and trim it
+myDfm <- dfm(tok)
 trimdfm <- dfm_trim(myDfm, min_termfreq = 10)
 
+# inspecting top features 
 topfeatures(trimdfm, n=20, decreasing = TRUE)
 
 set.seed(100)
-textplot_wordcloud(trimdfm, min_count = 100, color = c('red', 'pink', 'green', 'purple', 'orange', 'blue'))
+textplot_wordcloud(trimdfm, min_count = 100, 
+                   color = c('red', 'pink', 'green', 'purple', 'orange', 'blue'))
