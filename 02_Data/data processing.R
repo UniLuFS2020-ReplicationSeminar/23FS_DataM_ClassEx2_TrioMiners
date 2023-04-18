de98210dac9a63df7c824910862abfc715721f18
Benjamin Kurmann <benjamin@kurmail.ch>
2023-04-18 15:07:42 +0200

    adds definitiv empty-string/NA check. Not the reason for issue with not running.

--- 02_Data/data processing.R
+++ 02_Data/data processing.R
@@ -5,32 +5,36 @@ library(tidyverse)
 
 # I got an error creating the corpus from the improted dataframe:
 # dimnames <- fixupDN(.Object@Dimnames) : missing value where TRUE/FALSE needed
-
+# thats why I filter empty strings and NAs
 
 #load data
 load(here("AI_articles_guardian.RData"))
 
-
 # check for missing values and empty string + cleaning
-
-print(any(is.na(articles_df$V3)))
-print(any(articles_df$V3 == ""))
+any(is.na(articles_df))
+any_empty_strings <- any(apply(articles_df, 1, function(x) any(nchar(x) == 0)))
+any_empty_strings
 
 # cleaning data (removing html tags using regexpr)
 articles_df <- articles_df %>% 
+  mutate(V3 = gsub("<.*?>", "", V3)) %>% 
+  # NAs
   mutate(V1 = if_else(is.na(V1), "MISSING_TEXT", V1)) %>%  # Na hanlding
   mutate(V2 = if_else(is.na(V2), "MISSING_TEXT", V2)) %>%  # Na hanlding
   mutate(V3 = if_else(is.na(V3), "MISSING_TEXT", V3)) %>%  # Na hanlding
-  mutate(V3 = gsub("<.*?>", "", V3))
+  # empty stings
+  mutate(across(everything(), ~ if_else(.x == "", "MISSING_TEXT", .x), .names = 'new_{.col}'))
 
 # check for missing values and empty string (again)
-print(any(is.na(articles_df$V3))) # should return false
-print(any(articles_df$V3 == "")) # should return false
+any(is.na(articles_df))
+any_empty_strings <- any(apply(articles_df, 1, function(x) any(nchar(x) == 0)))
+any_empty_strings
 
 # create corpus and inspect it
 myCorpus <- corpus(articles_df, text_field = "V3")
 myCorpus <- corpus(myCorpus, docvars = select(articles_df, V1:V2))
 
+myCorpus
 
 head(summary(myCorpus))
 summary(myCorpus)
@@ -46,7 +50,7 @@ tok <- tokens_remove(tok, stopwords("en"))
 tok <- tokens_wordstem(tok)
 
 # create dfm and trim it
-myDfm <- dfm(tok, )
+myDfm <- dfm(tok)
 trimdfm <- dfm_trim(myDfm, min_termfreq = 10)
 
 # inspecting top features 
