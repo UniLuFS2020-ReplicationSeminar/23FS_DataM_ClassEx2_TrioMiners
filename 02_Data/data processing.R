432e893a6e674b5a60db8504b4fe58de57a35ffd
Benjamin Kurmann <benjamin@kurmail.ch>
2023-04-18 11:40:37 +0200

    adds na handling for all 3 columns

--- 02_Data/data processing.R
+++ 02_Data/data processing.R
@@ -3,18 +3,34 @@ library(quanteda)
 library(quanteda.textplots)
 library(tidyverse)
 
+# I got an error creating the corpus from the improted dataframe:
+# dimnames <- fixupDN(.Object@Dimnames) : missing value where TRUE/FALSE needed
+
 
 #load data
 load(here("AI_articles_guardian.RData"))
 
+
+# check for missing values and empty string + cleaning
+
+print(any(is.na(articles_df$V3)))
+print(any(articles_df$V3 == ""))
+
 # cleaning data (removing html tags using regexpr)
 articles_df <- articles_df %>% 
-  mutate(V3 = gsub("<.*?>", "", V3)) %>% 
-  mutate(V3 = if_else(is.na(V3), "MISSING_TEXT", V3)) # Na hanlding
+  mutate(V1 = if_else(is.na(V1), "MISSING_TEXT", V1)) %>%  # Na hanlding
+  mutate(V2 = if_else(is.na(V2), "MISSING_TEXT", V2)) %>%  # Na hanlding
+  mutate(V3 = if_else(is.na(V3), "MISSING_TEXT", V3)) %>%  # Na hanlding
+  mutate(V3 = gsub("<.*?>", "", V3))
+
+# check for missing values and empty string (again)
+print(any(is.na(articles_df$V3))) # should return false
+print(any(articles_df$V3 == "")) # should return false
 
 # create corpus and inspect it
 myCorpus <- corpus(articles_df, text_field = "V3")
 myCorpus <- corpus(myCorpus, docvars = select(articles_df, V1:V2))
+
 
 head(summary(myCorpus))
 summary(myCorpus)
@@ -30,7 +46,7 @@ tok <- tokens_remove(tok, stopwords("en"))
 tok <- tokens_wordstem(tok)
 
 # create dfm and trim it
-myDfm <- dfm(tok)
+myDfm <- dfm(tok, )
 trimdfm <- dfm_trim(myDfm, min_termfreq = 10)
 
 # inspecting top features 
