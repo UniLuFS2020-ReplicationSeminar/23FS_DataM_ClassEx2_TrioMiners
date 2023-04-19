b82fdd76c5111d71f3cd8c107086b412ef91c002
‚jullatt‘ <julien.nicolas@bluewin.ch>
2023-04-19 08:47:58 +0200

    add bar plot and export dfm

--- 02_Data/data processing.R
+++ 02_Data/data processing.R
@@ -19,9 +19,9 @@ any_empty_strings
 articles_df <- articles_df %>% 
   mutate(V3 = gsub("<.*?>", "", V3)) %>% 
   # NAs
-  mutate(V1 = if_else(is.na(V1), "MISSING_TEXT", V1)) %>%  # Na hanlding
-  mutate(V2 = if_else(is.na(V2), "MISSING_TEXT", V2)) %>%  # Na hanlding
-  mutate(V3 = if_else(is.na(V3), "MISSING_TEXT", V3)) %>%  # Na hanlding
+  mutate(V1 = if_else(is.na(V1), "MISSING_TEXT", V1)) %>%  # Na handling
+  mutate(V2 = if_else(is.na(V2), "MISSING_TEXT", V2)) %>%  # Na handling
+  mutate(V3 = if_else(is.na(V3), "MISSING_TEXT", V3)) %>%  # Na handling
   # empty stings
   mutate(across(everything(), ~ if_else(.x == "", "MISSING_TEXT", .x), .names = 'new_{.col}'))
 
@@ -31,8 +31,8 @@ any_empty_strings <- any(apply(articles_df, 1, function(x) any(nchar(x) == 0)))
 any_empty_strings
 
 # create corpus and inspect it
-myCorpus <- corpus(articles_df, text_field = "V3")
-myCorpus <- corpus(myCorpus, docvars = select(articles_df, V1:V2))
+myCorpus <- corpus(articles_df[1:200,], text_field = "V3") # include only 200 most relevant articles
+myCorpus <- corpus(myCorpus, docvars = select(articles_df[1:200,], V1:V2))
 
 myCorpus
 
@@ -53,9 +53,29 @@ tok <- tokens_wordstem(tok)
 myDfm <- dfm(tok)
 trimdfm <- dfm_trim(myDfm, min_termfreq = 10)
 
-# inspecting top features 
+# inspecting top features by plotting
 topfeatures(trimdfm, n=20, decreasing = TRUE)
 
+# word cloud
 set.seed(100)
 textplot_wordcloud(trimdfm, min_count = 100, 
                    color = c('red', 'pink', 'green', 'purple', 'orange', 'blue'))
+
+# frequency bars
+library("quanteda.textstats")
+features_dfm <- textstat_frequency(trimdfm, n = 100)
+
+# Sort by reverse frequency order
+features_dfm$feature <- with(features_dfm, reorder(feature, -frequency))
+
+ggplot(features_dfm, aes(x = feature, y = frequency)) +
+  geom_col(fill = "steelblue") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(title = "Corpus Top 100 features",
+       x = "Feature",
+       y = "Frequency")
+
+
+# export dfm
+save(trimdfm, file = here("AI_dfm_guardian.RData"))
+
